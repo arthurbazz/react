3a9fddca305316b1fe4ad293990126c9abcea64b
Chibuzor Arthur Uzoaru <chibuzorarthur@gmail.com>
2022-09-21 15:16:47 +0100

    add MouseEvent component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import React from "react";
 import CarsList from "./components/CarsList";
 import ChangeEvent from "./components/ChangeEvent";
 import ClickEvent from "./components/ClickEvent";
-import UserList from "./components/UserList";
+import MouseEvent from "./components/MouseEvent";
+// import UserList from "./components/UserList";
 import UserProfileCard from "./components/UserProfileCard";
 
 // create root component => App
 const App = () => {
+  // render...
   return (
     <section className="main-page">
       <UserProfileCard />
       <CarsList />
-      <UserList />
+      {/* <UserList /> */}
 
       {/* events */}
       <ClickEvent />
       <ChangeEvent />
+      <MouseEvent />
     </section>
   );
 };
